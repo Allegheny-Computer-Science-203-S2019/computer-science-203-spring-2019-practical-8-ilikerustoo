6024901456014b7a74807cad43e53225c68fd182
Mohammad Khan <mohammadnk1@gmail.com>
2019-04-29 10:23:06 -0400

    Push changes that I had not before by accident

--- plugins-src/plugins-compile-and-install.sh
+++ plugins-src/plugins-compile-and-install.sh
@@ -3,5 +3,6 @@ python3 -m compileall .
 mkdir -p ../plugins
 cp __pycache__/*.pyc ../plugins
 mv ../plugins/frequencies1.cpython-36.pyc ../plugins/frequencies1.pyc
+mv ../plugins/frequencies2.cpython-36.pyc ../plugins/frequencies2.pyc
 mv ../plugins/words1.cpython-36.pyc ../plugins/words1.pyc
 mv ../plugins/words2.cpython-36.pyc ../plugins/words2.pyc
